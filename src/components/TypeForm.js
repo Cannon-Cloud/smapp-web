79e29fa5686aae12f5230d747234ba2b84253d5b
clarence@cannon.cloud <clarence@cannon.cloud>
2021-09-23 10:24:49 -0700

    Demo

--- src/components/TypeForm.js
+++ src/components/TypeForm.js
@@ -1,12 +1,7 @@
 import { Popover } from "@typeform/embed-react";
 
 const TypeForm = () => {
-  return (
-    <Popover
-      id="oqyzUbgk"
-      tooltip="Interested in our product?<br/>Click here"
-    />
-  );
+  return <Popover id="oqyzUbgk" tooltip="Want a Free Demo?<br/>Click here" />;
 };
 
 export default TypeForm;
